977a3d71efa42e5b5cae35840a060237bcc10801
Snehan-Gn <pr46eml@eduvaud.ch>
2026-03-25 14:10:18 +0100

    initial commit

---
+++ P_335_ReadMe/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace P_335_ReadMe
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
