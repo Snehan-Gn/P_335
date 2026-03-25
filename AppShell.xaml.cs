e76410e2bdea30e37b25705ce793c7ccfe35b8da
Snehan-Gn <pr46eml@eduvaud.ch>
2026-03-25 13:32:41 +0100

    initial commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace P_335
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace P_335
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var decks = new List<Deck>
+        {
+            new Deck { Name = "Deck 1" },
+            new Deck { Name = "Deck 1" },
+            new Deck { Name = "Deck 1" }
+        };
+
+            DecksCollection.ItemsSource = decks;
+        }
+    }
+
+    public class Deck
+    {
+        public string Name { get; set; }
+    }
+
+}
