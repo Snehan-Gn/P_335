2ae9959dc1ab7fc932fbdf956529d45a2271d834
Snehan-Gn <pr46eml@eduvaud.ch>
2026-04-22 16:26:49 +0200

    feat(MainPage): Ajout d'une barre de recherche et de la fonction pour ajouter un Epub

---
+++ P_335_ReadMe/Services/DataService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+using P_335_ReadMe.Models;
+
+namespace P_335_ReadMe.Services
+{
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient;
+        private const string UrlApi = "http://10.0.2.2:3000/books";
+
+        public ApiService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<Book>> FetchBooksAsync()
+        {
+            try
+            {
+                return await _httpClient.GetFromJsonAsync<List<Book>>(UrlApi) ?? new();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Erreur : {ex.Message}");
+                return new List<Book>();
+            }
+        }
+    }
+}
